d8454e7e89c2f68166efac774e1d873e28472bf7
DESKTOP-LO4KPOR\moxsl <phantomchikk@gmail.com>
2017-12-27 02:19:34 +0300

    Add TestsManagerController
    Add TestsManager controller, actions: create, edit, delete.

--- Models/TestContext.cs
+++ Models/TestContext.cs
@@ -10,11 +10,11 @@ namespace WebApplication1.Models
     public class TestContext : IdentityDbContext<User>
     {
         public DbSet<Test> Tests { get; set; }
-        public DbSet<Section> Section { get; set; }
+        public DbSet<Section> Sections { get; set; }
         public DbSet<Question> Questions { get; set; }
         public DbSet<Answer> Answers { get; set; }
         public DbSet<Variant> Variants { get; set; }
-        public DbSet<Attempt> Attempt { get; set; }
+        public DbSet<Attempt> Attempts { get; set; }
         public TestContext(DbContextOptions<TestContext> options) : base (options)
         {
 
